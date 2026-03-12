6b73aac2dd283e0693871088e685e529373a4d02
Kusuma.B.J <kusumabj040406@gmail.com>
2026-03-12 20:36:25 +0530

    Create palindrome_number.java

---
+++ palindrome_number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+                String s = Integer.toString(x);
+        
+        String rev = new StringBuilder(s).reverse().toString();
+        
+        if(s.equals(rev)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+        
+    
